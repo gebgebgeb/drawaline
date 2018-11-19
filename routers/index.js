de9ee0317f2c3560895a93481c6d36c4e0c6c279
gebgebgeb <asdf>
2018-11-19 13:40:44 -0500

    console.logs to see whats going on on heroku

--- routers/index.js
+++ routers/index.js
@@ -11,9 +11,13 @@ const router = new Router()
 
 router.get('/all_templates', async (ctx, next) => {
 	await next()
+	console.log('we made it this far ################')
+	console.log(AXIOS_BASE_URL)
 	const dirnames = ['circle1', 'line0', 'line1', 'line2', 'line3']
 	let out = []
 	for(let dirname of dirnames){
+		console.log('dirname' + '****************')
+		console.log(AXIOS_BASE_URL + `/templates/${dirname}/metadata.json`)
 		let res = await axios.get(AXIOS_BASE_URL + `/templates/${dirname}/metadata.json`)
 		metadata = res.data
 		metadata.dirname = dirname
