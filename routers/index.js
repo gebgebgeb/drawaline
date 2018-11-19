e56219de66dcd770e9192a2b781c16c7a22eb7c7
gebgebgeb <asdf>
2018-11-19 13:44:04 -0500

    hopefully actually fixing 404 now

--- routers/index.js
+++ routers/index.js
@@ -4,20 +4,18 @@ const url = require('url')
 
 const PORT = process.env.PORT || 9999;
 const MY_URL = new URL(process.env.PRODUCTION_URL || 'http://localhost/')
-MY_URL.port = PORT
+if(!process.env.PORT){
+	MY_URL.port = PORT
+}
 const AXIOS_BASE_URL = MY_URL.origin
 
 const router = new Router()
 
 router.get('/all_templates', async (ctx, next) => {
 	await next()
-	console.log('we made it this far ################')
-	console.log(AXIOS_BASE_URL)
 	const dirnames = ['circle1', 'line0', 'line1', 'line2', 'line3']
 	let out = []
 	for(let dirname of dirnames){
-		console.log('dirname' + '****************')
-		console.log(AXIOS_BASE_URL + `/templates/${dirname}/metadata.json`)
 		let res = await axios.get(AXIOS_BASE_URL + `/templates/${dirname}/metadata.json`)
 		metadata = res.data
 		metadata.dirname = dirname
