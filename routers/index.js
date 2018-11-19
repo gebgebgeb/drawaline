b64b9aeae3d06d16e49fb9691e7f76b6c665d389
gebgebgeb <asdf>
2018-11-18 20:33:35 -0500

    fixing broken routes to templates

--- routers/index.js
+++ routers/index.js
@@ -2,6 +2,10 @@ const Router = require('koa-router')
 
 const axios = require('axios')
 
+const PORT = process.env.PORT || 9999;
+const URL = process.env.HEROKU_URL || 'http://localhost'
+const AXIOS_BASE_URL = URL + ':' + PORT
+
 const router = new Router()
 
 router.get('/all_templates', async (ctx, next) => {
@@ -9,7 +13,7 @@ router.get('/all_templates', async (ctx, next) => {
 	const dirnames = ['circle1', 'line0', 'line1', 'line2', 'line3']
 	let out = []
 	for(let dirname of dirnames){
-		let res = await axios.get(`http://localhost:9999/templates/${dirname}/metadata.json`)
+		let res = await axios.get(AXIOS_BASE_URL + `/templates/${dirname}/metadata.json`)
 		metadata = res.data
 		metadata.dirname = dirname
 		out.push(metadata)
